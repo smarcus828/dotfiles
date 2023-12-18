ca9856219ad5b76f7f70aa22ad7fc208813ba1d1
smarcus <smarcus@cicsnc.org>
2023-12-18 16:02:12 -0500

    add go_mac

---
+++ .go_mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chown -R smarcus /usr/local/var/homebrew
+sudo chown -R smarcus /usr/local/share/zsh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+git config --global user.name "Steven Marcus"
+git config --global user.email "smarcus@cicsnc.org"
+
+brew install stow zsh tree vim wget ncdu pgp pine iterm2
+
